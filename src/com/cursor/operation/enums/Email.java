900b7fa27ff392063a067b977f1505e574013a38
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-22 18:32:35 +0100

    init enums

---
+++ src/com/cursor/operation/enums/Email.java
@@ -0,0 +1,21 @@
+package com.cursor.operation.enums;
+
+public enum Email {
+    PETRO("petro@gmail.com"),
+    BARON("baron@gmail.com"),
+    AGATA("agata@gmail.com"),
+    BARTEK("bartek@gmail.com"),
+    BOGDAN("bogdan@gmail.com"),
+    DAN("dan164@gmail.com"),
+    MICOLAJ("micolaj@gmail.com");
+
+    private String email;
+
+    Email(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
