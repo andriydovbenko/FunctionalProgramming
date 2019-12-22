c56eba2e2e449fb6c1be4611a5b112bcf9728d8f
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-22 12:03:26 +0100

    created class Execution to execut project

---
+++ src/com/cursor/operation/Execution.java
@@ -0,0 +1,15 @@
+package com.cursor.operation;
+
+import com.cursor.operation.lambda.PrimeNumber;
+
+public class Execution {
+    public void start() {
+        int numberFirst = 1;
+        int numberSecond = 61;
+        int numberThird = -5;
+        System.out.println("Number: " + numberFirst + " is prime? =" + new PrimeNumber().isPrime(numberFirst));
+        System.out.println("Number: " + numberSecond + " is prime? =" + new PrimeNumber().isPrime(numberSecond));
+        System.out.println("Number: " + numberThird + " is prime? =" + new PrimeNumber().isPrime(numberThird));
+
+    }
+}
