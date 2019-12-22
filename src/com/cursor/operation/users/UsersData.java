3c6106505028a4fd0c1856753847dfdfec17cded
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-22 18:39:21 +0100

    created class UsersData with methods

---
+++ src/com/cursor/operation/users/UsersData.java
@@ -0,0 +1,51 @@
+package com.cursor.operation.users;
+
+import com.cursor.operation.enums.Email;
+import com.cursor.operation.enums.Team;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UsersData {
+    private final int YEAR = 2019;
+    private final int MONTH = 12;
+    private final int DAY = 15;
+    private List<User> userList = new ArrayList<>();
+
+    public UsersData() {
+        createListOfUsers();
+    }
+
+    public void createListOfUsers() {
+        userList.add(new User(Email.PETRO.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_LAD.getTeam()));
+        userList.add(new User(Email.BARON.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_RAB.getTeam()));
+        userList.add(new User(Email.AGATA.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_RIM.getTeam()));
+        userList.add(new User(Email.BARTEK.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_LAD.getTeam()));
+        userList.add(new User(Email.BOGDAN.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_RAB.getTeam()));
+        userList.add(new User(Email.DAN.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_RIM.getTeam()));
+        userList.add(new User(Email.MICOLAJ.getEmail(),
+                LocalDate.of(YEAR, MONTH, new Random().nextInt(DAY) + 1), Team.TEAM_LAD.getTeam()));
+    }
+
+    public void showAllUsers() {
+        System.out.println("\nUser's list:");
+        userList.forEach(x -> System.out.println("email: " + x.getEmail() +
+                " team: " + x.getTeam() + " last logged: " + x.getLoginDate()));
+    }
+
+    public void filterUser() {
+        System.out.println("\nEmails of user who logged less than week age grouped by team:");
+        userList.stream()
+                .filter(login -> login.getLoginDate().getDayOfMonth() >= (DAY - 7))
+                .collect(Collectors.groupingBy(User::getTeam, Collectors.mapping(User::getEmail, Collectors.toList())))
+                .entrySet()
+                .forEach(System.out::println);
+    }
+}
