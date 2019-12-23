488cde78409b90e9680cf40118472b9fd6cabb09
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-23 08:42:37 +0100

    crated class Film

---
+++ src/com/cursor/operation/film/Film.java
@@ -0,0 +1,11 @@
+package com.cursor.operation.film;
+
+public class Film {
+    private float ticketPrice;
+    private String genre;
+
+    public Film(float ticketPrice, String genre) {
+        this.ticketPrice = ticketPrice;
+        this.genre = genre;
+    }
+}
