7a9e49dea6ea5f8dd881bd4d3168fd6fa6974941
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-23 12:33:05 +0100

    corrected film's prices

--- src/com/cursor/operation/enums/Films.java
+++ src/com/cursor/operation/enums/Films.java
@@ -4,19 +4,19 @@ public enum Films {
 
     THUNDERBALL("Action", "20.20"),
     SPECTRE("Action", "18.60"),
-    DIAMONDS("Action", "22.55"),
+    DIAMONDS("Action", "22.50"),
     TWICE("Action", "16.30"),
     SLAPSTICK("Comedy", "23.30"),
-    ACEVENTURA("Comedy", "16.25"),
+    ACEVENTURA("Comedy", "16.20"),
     MADWORLD("Comedy", "24.80"),
     THEMASK("Comedy", "21.70"),
     MERTOPOLIS("Fantasy", "19.60"),
     WIZARD("Fantasy", "15.20"),
     HORIZONT("Fantasy", "16.60"),
     KINGKONG("Fantasy", "17.80"),
-    UNFORGIVEN("Western", "18.44"),
-    LITTLEMAN("Western", "19.88"),
-    HUD("Western", "17.55"),
+    UNFORGIVEN("Western", "18.40"),
+    LITTLEMAN("Western", "19.80"),
+    HUD("Western", "17.50"),
     PURSUED("Western", "14.50");
 
     private String[] genreAndPrice;
