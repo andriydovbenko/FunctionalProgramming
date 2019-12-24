ac446f9447de3c5c19ea94c7901af902d74d0a25
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-24 17:31:50 +0100

    removed redundant fild

--- src/com/cursor/operation/film/FilmsCollection.java
+++ src/com/cursor/operation/film/FilmsCollection.java
@@ -4,7 +4,6 @@ import com.cursor.operation.film.list.Films;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
