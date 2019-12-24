a5c48e9a1fa8ac76af1b8a28ec08f8af7cf2652b
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-24 16:54:35 +0100

    refactored code

--- src/com/cursor/operation/Execution.java
+++ src/com/cursor/operation/Execution.java
@@ -1,17 +1,15 @@
 package com.cursor.operation;
 
 import com.cursor.operation.film.FilmsCollection;
-import com.cursor.operation.prime.PrimeNumber;
+import com.cursor.operation.prime.PrimeNumberAuditor;
 import com.cursor.operation.users.UsersData;
 
 public class Execution {
+
     public void start() {
-        int numberFirst = 1;
-        int numberSecond = 61;
-        int numberThird = -5;
-        System.out.println("Number: " + numberFirst + " is prime? =" + new PrimeNumber().isPrime(numberFirst));
-        System.out.println("Number: " + numberSecond + " is prime? =" + new PrimeNumber().isPrime(numberSecond));
-        System.out.println("Number: " + numberThird + " is prime? =" + new PrimeNumber().isPrime(numberThird));
+        new PrimeNumberAuditor().checkNumber(1);
+        new PrimeNumberAuditor().checkNumber(5);
+        new PrimeNumberAuditor().checkNumber(-10);
 
         UsersData usersData = new UsersData();
         usersData.showAllUsers();
