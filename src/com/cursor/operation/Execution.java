6863c9ebf19d7e79590a4c4764d501f8e4de29be
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-25 08:58:07 +0100

    changed execution first task

--- src/com/cursor/operation/Execution.java
+++ src/com/cursor/operation/Execution.java
@@ -1,15 +1,13 @@
 package com.cursor.operation;
 
 import com.cursor.operation.film.FilmsCollection;
-import com.cursor.operation.prime.PrimeNumberAuditor;
+import com.cursor.operation.prime.SquareRootAuditor;
 import com.cursor.operation.user.UsersData;
 
 public class Execution {
 
     public void start() {
-        new PrimeNumberAuditor().checkNumber(1);
-        new PrimeNumberAuditor().checkNumber(5);
-        new PrimeNumberAuditor().checkNumber(-10);
+        SquareRootAuditor.findSquareRoot();
 
         UsersData usersData = new UsersData();
         usersData.showAllUsers();
